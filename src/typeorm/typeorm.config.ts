19f528cd6c8dbd0c53e0bd3dcd99bb71798e43e8
codeinvisible <lamasandesh400@gmail.com>
2023-01-03 23:33:04 +0545

    login issue fixed

--- src/typeorm/typeorm.config.ts
+++ src/typeorm/typeorm.config.ts
@@ -2,13 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'sandeshtamang',
-  database: 'blog',
-  entities: [__dirname + '/../**/models/entity/*.entity{.ts,.js}'],
-  // url: process.env.DATABASE_URL,
-  // autoLoadEntities: true,
+  // host: 'localhost',
+  // port: 5432,
+  // username: 'postgres',
+  // password: 'sandeshtamang',
+  // database: 'blog',
+  // entities: [__dirname + '/../**/models/entity/*.entity{.ts,.js}'],
+  url: process.env.DATABASE_URL,
+  autoLoadEntities: true,
   synchronize: true,
 };
